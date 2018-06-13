fab71260f1b3abb321013c9fa78a4966e9b16f6a
Terry Yang <terry.yang@kooppi.com>
2018-06-13 17:24:09 +0800

    Braintree test
    信用卡驗證lib test

---
+++ Pods/Target Support Files/Pods-payTest/Pods-payTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_payTest : NSObject
+@end
+@implementation PodsDummy_Pods_payTest
+@end
--- payTest/ViewController.swift
+++ payTest/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import BraintreeDropIn
+import Braintree
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnShow: UIButton!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchClientToken() {
+        self.btnShow.isEnabled = false
+        self.indicator.isHidden = false
+        // TODO: Switch this URL to your own authenticated API
+        let clientTokenURL = NSURL(string: "https://braintree-sample-merchant.herokuapp.com/client_token")!
+        let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
+        clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
+            // TODO: Handle errors
+            let clientToken = String(data: data!, encoding: String.Encoding.utf8)
+            print("clientToken:\(clientToken!)")
+            self.showDropIn(clientTokenOrTokenizationKey: clientToken!)
+            self.btnShow.isEnabled = true
+            self.indicator.isHidden = true
+            // As an example, you may wish to present Drop-in at this point.
+            // Continue to the next section to learn more...
+            }.resume()
+    }
 
+    @IBAction func jumpDropIn(_ sender: UIButton) {
+        fetchClientToken()
+    }
+    
+    
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request) { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR:\(error.debugDescription)")
+            } else if (result?.isCancelled == true) {
+                print("CANCELLED")
+            } else if let result = result {
+                // Use the BTDropInResult properties to update your UI
+                print("result paymentOptionType:\(result.paymentOptionType)")
+                print("result paymentMethod:\(result.paymentMethod.debugDescription)")
+                print("result paymentIcon:\(result.paymentIcon)")
+                print("result paymentDescription:\(result.paymentDescription)")
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        self.present(dropIn!, animated: true, completion: nil)
+    }
 }
 
